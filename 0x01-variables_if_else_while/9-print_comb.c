29d3e391c06f45383cb0d0edd6706c7331af3108
Wilson Aballey <willywallet001@gmail.com>
2022-12-09 15:47:10 -0800

    .

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -14,6 +13,7 @@ int main(void)
 		if (d != '9')
 		{	
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
