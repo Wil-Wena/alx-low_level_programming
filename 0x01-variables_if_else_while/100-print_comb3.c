f138f777ba1626dbeb54f74cf6008db8d7d532ab
Wilson Aballey <willywallet001@gmail.com>
2022-12-09 15:56:42 -0800

    .

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 90; d++)
+	{
+		for ()
+		{
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+		}
+		if (d != 89)
+		{
+			putchar(',');		
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
