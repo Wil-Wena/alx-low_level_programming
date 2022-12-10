cdaf8419d4249e71b56342d796d5ba3e192b763f
Wilson Aballey <willywallet001@gmail.com>
2022-12-09 16:10:06 -0800

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
-
-
+#include <stdio.h
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int a;
