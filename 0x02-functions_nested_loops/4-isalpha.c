114892d5a9b9f4e92ecafa0b5ca83d9107ae348f
Wilson Aballey <willywallet001@gmail.com>
2022-12-13 15:02:58 -0800

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,8 @@
  * Return: 1 for alphabetic character or 0 for anything else              */                                       
                                                                     
 int _isalpha(int c)                                                      
-
 {                                                                        
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))                                         
-        {                                                                
+        if ( (c >= 65 && c <= 90) || (c >= 97 && c <= 122) )                     {                                                                
                 return (1);                                              
         }                                                                
         return (0);                                                      
