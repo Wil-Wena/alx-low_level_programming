84e0c51e8819d33e52e43a513c6281f4fcbad446
Wilson Aballey <willywallet001@gmail.com>
2022-12-19 16:30:57 -0800

    .

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,13 @@
 void print_array(int *a, int n)
 {
 
-	int i;
+	int i = 0;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
 	printf("\n");
 }
