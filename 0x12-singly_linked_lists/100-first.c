1a0ddd5593cf27f8405dda76fda44b167fb597e0
Wilson Aballey <willywallet001@gmail.com>
2023-01-27 14:43:20 -0800

    .

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * first - prints before main
+ */
+
+void __attribute__ ((constructor)) first()
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
